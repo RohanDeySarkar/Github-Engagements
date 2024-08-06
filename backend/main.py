423960da1932d38e84228eafcd90ffeef692a528
unknown <deysarkarrohan@gmail.com>
2024-08-06 17:41:11 +0530

    adding backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
 # api's
 # top 10 open source repo
 # check their data ...
-
+# push data to db
+#  frontned will read from db create visualizations
+# jenkins job will run python file periodically to collect data adn send to db
+# select db
+# later part.. -> create a mlops solution
+# parts covered in this project -> full stack, data analysis, ml, ci/cd
