22dfe96ae9c14275c3882cb0554c42864bd84aac
unknown <deysarkarrohan@gmail.com>
2024-08-10 18:28:03 +0530

    github api data checking..

--- backend/main.py
+++ backend/main.py
@@ -14,3 +14,34 @@
 
 # flask api's add here
 # to fetch data for a particular publics repo
+
+from github import Github
+from github import Auth
+
+import config
+
+
+class GithubData:
+    def __init__(self, token):
+        auth = Auth.Token(token)
+        self.g = Github(auth=auth)
+        self.g.get_user().login
+        self.github_data = []
+
+    def fetch_data(self):
+        pass
+
+
+
+    # def send_data_to_db(self):
+    #     pass
+
+
+def collect_github_data():
+    github_data = GithubData(config.token)
+    github_data.fetch_data()
+
+
+collect_github_data()
+# if __name__ == "main":
+#     fetch_github_data()
--- backend/test.py
+++ backend/test.py
@@ -10,14 +10,88 @@ g.get_user().login
 # list of all top repos
 # https://analyticsindiamag.com/ai-origins-evolution/12-most-popular-open-source-projects-on-github/
 
+github_data = []
 
-repo = g.get_repo("tensorflow/tensorflow")
-print(repo.get_topics())
+# just list competetive repos ?? Eg: tensorflow vs pytorch
+# TOP ten AI libraries
+repo_list = [
+    "tensorflow/tensorflow",
+    "pytorch/pytorch"
+    "opencv/opencv",
+    "scikit-learn/scikit-learn",
+    "keras-team/keras",
+    "langchain-ai/langchain",
+    "Artelnics/opennn",
+    "huggingface/transformers",
+    "openai/whisper",
+    "dmlc/xgboost",
+    "BVLC/caffe",
+    "apache/mxnet"
+    "deeplearning4j/deeplearning4j"
+]
+
+for repo in repo_list:
+    try:
+        repo = g.get_repo(repo)
+        # print(dir(repo))
+        print(repo.full_name)
+        # print(repo.description)
+        # print(repo.forks_count)
+        # print(repo.created_at)
+        # print(repo.updated_at)
+        # print(repo.homepage)
+        # print(repo.url)
+        # print(repo.subscribers_count)
+        # print("SIZE", repo.size)
+        # print(repo.get_comments())
+        # print(repo.get_top_referrers())
+        # contents = repo.get_views_traffic()
+        # contents = repo.get_views_traffic(per="week")
+        # print(contents)
+        break
+
+        # topics = repo.get_topics()    
+        # open_issues = repo.get_issues(state='open')
+
+        # latest_open_issues = []
+        # count = 0
+
+        # for issue in open_issues:
+        #     if count == 10:
+        #         break
+        #     latest_open_issues.append(issue.title)
+        #     count += 1
+
+        temp_data = {
+            "repo_name" : repo.full_name,
+            "description:": repo.description,
+            "topics" : topics,
+            "stars" : repo.stargazers_count,
+            "watchers_count" : repo.watchers_count,
+            "forks" : repo.forks_count,
+            "creatd_at" : repo.created_at,
+            "updated_at" : repo.updated_at,
+            "homepage" : repo.homepage,
+            "latest_open_issues" : latest_open_issues
+        }
+        # github_data.append(temp_data)
+    except:
+        print(f'{repo} not found')
+
+print(github_data)
+
 
 # https://pygithub.readthedocs.io/en/stable/examples/Repository.html#get-repository-topics
 
+# Home page of UI these data needed (TOP 10 repo engagements)
 # number of prs
 # number of contributors
 # lines of codes
 # list of open issues
 #  
+
+# Individual page UI these data needed (USER CONTRIBUTION..)
+# lines of code
+# number of repos
+# open source contribuytion
+# medals ??
